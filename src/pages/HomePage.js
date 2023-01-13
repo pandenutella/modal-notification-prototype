42226856a58712df5a46021e16f4490c14815538
johnemmanuelb <johnemmanuel.bacalla@gmail.com>
2023-01-13 10:56:53 +0800

    Make the trigger button rendering dynamic

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,32 @@
-import { Button } from "antd";
+import { Button, Space, Typography } from "antd";
 import useNotifications from "../hooks/useNotifications";
 
 const HomePage = () => {
-  const { showManual } = useNotifications();
+  const { notifications, showManual } = useNotifications();
 
-  const handleClick = () => showManual("template");
+  const manualNotifications = notifications.filter(
+    (notification) => !notification.automatic
+  );
 
-  return <Button onClick={handleClick}>Display Template Notification</Button>;
+  const handleClick = (key) => () => showManual(key);
+
+  return (
+    <>
+      <Typography.Paragraph>
+        Click to trigger the following Manual Notification(s):
+      </Typography.Paragraph>
+      <Space>
+        {manualNotifications.map((notification) => (
+          <Button
+            key={notification.key}
+            onClick={handleClick(notification.key)}
+          >
+            {notification.name}
+          </Button>
+        ))}
+      </Space>
+    </>
+  );
 };
 
 export default HomePage;
